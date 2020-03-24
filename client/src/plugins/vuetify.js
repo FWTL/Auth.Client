80eac5b243aaa4a8d272537388facfb97fbd772d
Andrzej Golaszewski <agolasz@softserveinc.com>
2020-03-24 21:44:12 +0100

    project setup

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,18 +1,22 @@
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi'
+  },
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#ee44aa',
+        primary: '#5682a3',
         secondary: '#424242',
-        accent: '#82B1FF',
+        accent: '#dfe5ec',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
---
+++ client/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app class="accent">
+    <v-content>
+      <v-container class="fill-height" color="primary" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Registration form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Email" name="email" prepend-icon="mdi-email" type="email" />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+
+                  <v-text-field
+                    id="repeatPassword"
+                    label="Repeat Password"
+                    name="repeatPassword"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  }
+};
+</script>
